aeb6d50845d5c36481e0ac32f5acfd8f6b388744
Alessandro Rios <drios28@outlook.es>
2023-03-30 14:28:01 -0500

    feat: add google recaptcha
    add google recaptcha

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import { useEffect, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import RootLayout from './layout'
@@ -22,15 +23,19 @@ export default function App({
     setLoading(loading)
   }, [loading, setLoading])
 
+  const reCaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-      <Toaster />
-    </SessionContextProvider>
+    <ReCaptchaProvider reCaptchaKey={reCaptchaKey}>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+        <Toaster />
+      </SessionContextProvider>
+    </ReCaptchaProvider>
   )
 }
 
