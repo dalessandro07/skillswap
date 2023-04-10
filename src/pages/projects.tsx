cbf38c79818f63e7ce59bc78736a74e50a2b1eb8
Alessandro Rios <drios28@outlook.es>
2023-04-10 08:55:20 -0500

    fix: add new project link in mobile

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import ProjectListContainer from '@/components/projects/ProjectListContainer'
+import Link from 'next/link'
 
 export default function ProjectsPage() {
   return (
@@ -14,6 +15,12 @@ export default function ProjectsPage() {
 
       <section className="flex flex-col justify-center grow gap-8 py-10">
         <h1 className="text-center text-4xl font-bold">Proyectos</h1>
+
+        <Link
+          className="flex justify-center sm:hidden bg-orange-500 py-2 rounded-sm hover:bg-transparent hover:text-orange-500 transition-colors duration-200 hover:border-orange-600 border-b border-transparent"
+          href="/projects/new_project">
+          Nuevo proyecto
+        </Link>
 
         <ProjectListContainer />
       </section>
