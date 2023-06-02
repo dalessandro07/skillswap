32d85ba52b531113acf8d1116efbc1f4c5dafb57
Alessandro Rios <drios28@outlook.es>
2023-06-02 09:25:12 -0500

    style: add projects length and increase font size of heading

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import ProjectListContainer from '@/components/projects/ProjectListContainer'
 import Link from 'next/link'
+import { useProjectsStore } from '@/context/useProjectsStore'
 
 export default function ProjectsPage() {
+  const { projects } = useProjectsStore()
+
+  const pQuantity = projects.length
+
   return (
     <>
       <Head>
@@ -14,7 +19,19 @@ export default function ProjectsPage() {
       </Head>
 
       <section className="flex flex-col justify-center grow gap-8 py-10">
-        <h1 className="text-center text-4xl font-bold">Proyectos</h1>
+        <header className="flex flex-col gap-1 items-center">
+          <h1 className="text-center text-5xl font-bold">Proyectos</h1>
+
+          <p className="text-center text-gray-500">
+            {pQuantity === 0 ? (
+              'No hay proyectos'
+            ) : (
+              <>
+                {pQuantity} proyecto{pQuantity > 1 ? 's' : ''}
+              </>
+            )}
+          </p>
+        </header>
 
         <Link
           className="flex justify-center sm:hidden bg-orange-500 py-2 rounded-sm hover:bg-transparent hover:text-orange-500 transition-colors duration-200 hover:border-orange-600 border-b border-transparent"
